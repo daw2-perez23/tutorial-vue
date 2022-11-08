38b9a2f0c4e1e19aabf2f67486711bc8e32b5144
daw2-perez23 <perezabellanalex@fpllefia.com>
2022-11-08 20:33:01 +0100

    v7.3

---
+++ src/components/FormularioPersona.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="formulario-persona">
+    <form>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Nombre</label>
+              <input type="text" class="form-control" />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Apellido</label>
+              <input type="text" class="form-control" />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Email</label>
+              <input type="email" class="form-control" />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <button class="btn btn-primary">Añadir persona</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'formulario-persona',
+    data() {
+      return {
+        persona: {
+          nombre: '',
+          persona: '',
+          apellido: '',
+        },
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
